c6e6403d67c0d20f2aa46a778649fb34b5d2efe0
Javier Olazaran <jolazaran@flexiti.com>
2021-03-21 20:03:45 -0300

    Added SSR

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingModule) }, { path: 'combat-calculator', loadChildren: () => import('./pages/combat-calculator/combat-calculator.module').then(m => m.CombatCalculatorModule) }, { path: 'dungeon', loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule) }];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full'},
+      {
+        path: 'home',
+        loadChildren: () => import('./pages/landing/landing.module').then(m => m.LandingModule)
+      },
+      {
+        path: 'combat-calculator',
+        loadChildren: () => import('./pages/combat-calculator/combat-calculator.module').then(m => m.CombatCalculatorModule)
+      },
+      {
+        path: 'dungeon',
+        loadChildren: () => import('./pages/admin/admin.module').then(m => m.AdminModule)
+      }
+    ]
+  },
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabled'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
