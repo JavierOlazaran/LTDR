5f8c98a76bae5307806badec91ee804d6af4fd8c
jjsegovia <jsegovia@gmail.com>
2021-10-01 21:04:01 -0300

    monster-card creation

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { CombatCalculatorBtnComponent } from './components/combat-calculator-btn/combat-calculator-btn.component';
+import { MonsterCardComponent } from './components/monster-card/monster-card.component';
 
 @NgModule({
-  declarations: [SideNavComponent, CombatCalculatorBtnComponent],
+  declarations: [SideNavComponent, CombatCalculatorBtnComponent, MonsterCardComponent],
   imports: [
     CommonModule
   ],
   exports: [
     SideNavComponent,
-    CombatCalculatorBtnComponent
+    CombatCalculatorBtnComponent,
+    MonsterCardComponent
   ]
 })
 export class SharedModule { }
