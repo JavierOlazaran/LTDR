4e5c142550c590a8599bddaa61461181d3046ff8
Javier Olazaran <jjolazaran@gmail.com>
2022-03-28 11:49:31 -0300

    Added initial styles to mobile nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { SharedModule } from './shared/shared.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {SharedModule} from './shared/shared.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule.withServerTransition({appId: 'serverApp'}),
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/shared/components/side-nav/nav-bar-mobile/nav-bar-mobile.component.ts
+++ src/app/shared/components/side-nav/nav-bar-mobile/nav-bar-mobile.component.ts
@@ -1,16 +1,41 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-nav-bar-mobile',
   templateUrl: './nav-bar-mobile.component.html',
   styleUrls: ['./nav-bar-mobile.component.scss'],
+  animations: [
+    trigger('expand', [
+      transition('void => *', [
+        style({height: 0}),
+        animate(200)
+      ])
+    ]),
+
+    trigger('fold', [
+      transition('* => void', [
+          animate(200,
+            style({height: 0})
+          )
+        ]
+      )
+    ])
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavBarMobileComponent implements OnInit {
 
-  constructor() { }
+  isNavbarExpanded = false;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  onToggleClick(): void {
+    this.isNavbarExpanded = !this.isNavbarExpanded;
   }
 
 }
