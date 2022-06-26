27e563b6ccb437c183e9b5b61c77e171d169f17b
Javier Olazaran <jjolazaran@gmail.com>
2022-06-26 14:16:05 -0300

    Added basic layout and styles to the content display section

--- src/app/pages/landing/landing.module.ts
+++ src/app/pages/landing/landing.module.ts
@@ -1,19 +1,32 @@
-import { SharedModule } from './../../shared/shared.module';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {SharedModule} from '../../shared/shared.module';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { LandingRoutingModule } from './landing-routing.module';
-import { LandingComponent } from './landing.component';
-import { ScheduleBlockComponent } from './components/schedule-block/schedule-block.component';
-import { ScheduleItemComponent } from './components/schedule-item/schedule-item.component';
+import {LandingRoutingModule} from './landing-routing.module';
+import {LandingComponent} from './landing.component';
+import {ScheduleBlockComponent} from './components/schedule-block/schedule-block.component';
+import {ScheduleItemComponent} from './components/schedule-item/schedule-item.component';
+import {MediaContentDisplaySectionComponent} from './components/media-content-display-section/media-content-display-section.component';
+import {
+  MediaContentDisplayCarrouselComponent
+} from './components/media-content-display-carrousel/media-content-display-carrousel.component';
+import {MediaContentDisplayElementComponent} from './components/media-content-display-element/media-content-display-element.component';
 
 
 @NgModule({
-  declarations: [LandingComponent, ScheduleBlockComponent, ScheduleItemComponent],
+  declarations: [
+    LandingComponent,
+    ScheduleBlockComponent,
+    ScheduleItemComponent,
+    MediaContentDisplaySectionComponent,
+    MediaContentDisplayCarrouselComponent,
+    MediaContentDisplayElementComponent
+  ],
   imports: [
     CommonModule,
     LandingRoutingModule,
     SharedModule
   ]
 })
-export class LandingModule { }
+export class LandingModule {
+}
