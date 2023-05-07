c299ddf906fc73961edcf68e0402d129034b7f1a
Javier Olazaran <jjolazaran@gmail.com>
2023-05-07 19:14:49 -0300

    Updated angular version v14 -> v15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,8 +22,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabledBlocking',
-    relativeLinkResolution: 'legacy'
+    initialNavigation: 'enabledBlocking'
 })],
   exports: [RouterModule]
 })
